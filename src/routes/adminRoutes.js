2c0dfa09316e417bac6a39825ab514433c825748
fadymohyeldeen <fadymohy@zohomail.com>
2025-05-28 00:50:10 +0300

    fixing deployment issues

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,12 +1,12 @@
-import express from "express";
-import { login } from "../services/adminServices.js";
+  import express from "express";
+  import { login } from "../services/adminServices.js";
 
-const router = express.Router();
+  const router = express.Router();
 
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const { data, statusCode } = await login({ email, password });
-  res.status(statusCode).send(data);
-});
+  router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const { data, statusCode } = await login({ email, password });
+    res.status(statusCode).send(data);
+  });
 
-export default router;
+  export default router;
